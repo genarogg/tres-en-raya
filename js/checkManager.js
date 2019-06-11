15cf51afa3fcd96931472237e8666f5942cd371d
genarogg <gena_gonzalez15@hotmail.com>
2019-06-10 20:58:33 -0400

    for para checkRow ()

--- js/checkManager.js
+++ js/checkManager.js
@@ -1,7 +1,11 @@
 function checkRow(x, value){
     var countValue = 0;
 
-    
+    for (i = 0; i < 3; i++) {
+        if(board[i][x] == value){
+            countValue++;
+        }	
+    }
     return countValue;
 }
 
