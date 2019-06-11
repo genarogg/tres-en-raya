cf6827e0d0c423ccc9de2ba22bcc572e8dee07a9
genarogg <gena_gonzalez15@hotmail.com>
2019-06-10 20:51:55 -0400

    condiciones showMessage ()

--- js/messagemanager.js
+++ js/messagemanager.js
@@ -27,5 +27,14 @@ function showMessage(winner){
     MessageButton = document.getElementById('button');
     MessageButton.innerHTML = stringButton;
 
-   
+    if(winner == 1){
+        StringTwitter = "<a class='tweet' target='blank' href='http://facebook.com/share?text=se me hizo imposible ganar este juegos genarogg.ml/juegos/Tres-en-raya'>compartir por Facebook</a>";
+    }
+
+    else{
+        StringTwitter = "<a class='tweet' target='blank' href='http://facebook.com/share?text=he ganado ha un juego con inteligencia artificial genarogg.ml/juegos/Tres-en-raya'>compartir por Facebook</a>";
+
+    }
+    messageTweet = document.getElementById('tweetGameOver');
+    messageTweet.innerHTML = StringTwitter;
 }
