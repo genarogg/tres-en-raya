5e2995337635ffcccbe9965646ad3b4e16fc0114
genarogg <gena_gonzalez15@hotmail.com>
2019-06-10 20:51:20 -0400

    condiciones showMessage ()

--- js/messagemanager.js
+++ js/messagemanager.js
@@ -7,7 +7,16 @@ function showMessage(winner){
     panelMessage = document.getElementById('message');
     panelMessage.style.display = "block";
 
-    
+    if(winner == 1) {
+        stringNotification = "Game Over";
+    }
+    else{
+        stringNotification = "You win";
+    }
+
+    notificationMessage = document.getElementById('notification');
+    notificationMessage.innerHTML = stringNotification;
+
   
    
 }
