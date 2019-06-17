bb99b494327d2c115d1643aab5780d0f945634a4
genarogg <gena_gonzalez15@hotmail.com>
2019-06-16 21:51:28 -0400

    condicion sino -> bloquear linea

--- js/smart.js
+++ js/smart.js
@@ -157,7 +157,9 @@ function completeRow(x, turnValue){
                         var x = Math.round(Math.random() * 2);
                         var y = Math.round(Math.random() * 2);
 
-                        
+                        if(board[x][y] == 1 && checkBlock(x, y) == false){
+                            cellFind = true;
+                        }
                     }
                     
 
