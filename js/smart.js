f55108979366b3ee7add14e2d29c339495745c98
genarogg <gena_gonzalez15@hotmail.com>
2019-06-19 22:02:10 -0400

    buscamos una ficha de la maquina que no bloquea

--- js/smart.js
+++ js/smart.js
@@ -311,7 +311,9 @@ function completeDiag(x, turnValue){
                         var x = Math.round(Math.random() * 2);
                         var y = Math.round(Math.random() * 2);
 
-                        
+                        if(board[x][y] == 1 && checkBlock(x, y) == false){
+                            cellFind = true;
+                        }
                     }
                     
                 } 
