b2d485da94d7219fc1beb07cfef1b993074eadb9
genarogg <gena_gonzalez15@hotmail.com>
2020-02-15 22:05:42 -0400

    button

--- js/buttons.js
+++ js/buttons.js
@@ -7,3 +7,4 @@ function buttonFollowerMe() {
   var FollowerMe =
     "<a href='https://twitter.com/share' class='twitter-share-button' data-via='JoseCodFacilito' data-size='large' data-count='none' data-text='Juego de 3 en Raya | Tic Tac Toe Game | Noughts & Crosses Game '>Tweet</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>";
 }
+
